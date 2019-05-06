a478b0fdb9055be3d6e19d4bdf41c6101084cb2b
Kelsy <kelsywells@gmail.com>
2019-05-05 20:53:28 -0700

    Just realized I didn't have LESS open for a large chunk of this. AHHH! Turning it on as I finish propagation. Last step.

--- js/index.js
+++ js/index.js
@@ -5,7 +5,12 @@
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('mouseover', function(){
     logoHeading.style.color = 'red';
+    stopPropagation();
 });
+
+logoHeading.addEventListener('mouseover', function(){
+    logoHeading.style.backgroundColor = 'red';
+})
 
 //keypress:
 
@@ -20,19 +25,82 @@ console.log(meanText);
 
 //drag:
 
-const busImg = document.querySelector('img');
-console.log(busImg);
-busImg.addEventListener('drag', function(event){
-}, false);
+// const busImg = document.querySelector('img');
+// console.log(busImg);
+// busImg.addEventListener('drag', function(event){
+// }, false);
 
 //click:
 
-const header2 = document.querySelectorAll('h2');
-console.log(header2);
-header2.forEach(header2 => function reverseString(){
-    return(header2);
+    //couldn't make reverseString work so it's commented out. halp
+        // const header2 = document.querySelectorAll('h2');
+        // console.log(header2);
+        // header2.forEach(header2 => function reverseString(){
+        //     return(header2);
+        // });
+        // header2.addEventListener('click', function(){
+        // return header2.reverseString();
+        // });
+
+const header = document.querySelector('header');
+header.addEventListener('click', function(){
+    header.style.backgroundColor= 'blue'
+})
+console.log(header);
+
+//doubleclick:
+
+const footer = document.querySelector('footer');
+footer.addEventListener('dblclick', function(){
+    const newPTag = document.createElement('p');
+    newPTag.textContent = 'Please dont copy our name!'
+    footer.prepend(newPTag);
 });
-header2.addEventListener('keydown', function(){
-return header2.reverseString;
+
+
+//load:
+
+const headerTxt = document.querySelector('header');
+window.addEventListener('load', function(){
+    const newSpanTag = document.createElement('span');
+    newSpanTag.textContent = 'YES! It loaded!'
+    newSpanTag.style.fontSize = '36px'
+    headerTxt.append(newSpanTag);
+})
+
+//copy/paste:
+
+const source = document.querySelector('div.source');
+const target = document.querySelector('div.target');
+
+source.style.fontSize='24px'
+target.style.fontSize='24px'
+
+source.addEventListener('copy', function(event) {
+    const selection = document.getSelection();
+    event.clipboardData.setData('text/plain', selection.toString().textContent = 'WHAT DID I TELL YOU ABOUT COPYING? (double-click copyright to find out)');
+    event.preventDefault();
 });
-console.log(header2);
+
+//submit
+    
+const form = document.getElementById('form');
+const log = document.getElementById('log');
+form.addEventListener('submit', function logSubmit(event) {
+    log.textContent = `You submitted your answer! Nice!`;
+    event.preventDefault();
+  });
+
+  //reset
+
+form.addEventListener('reset', function(){
+    log.textContent = `go ahead now.`
+  })
+
+  //contextmenu
+
+noContext = document.getElementById('noContextMenu');
+
+noContext.addEventListener('contextmenu', function(e) {
+  e.preventDefault();
+});
